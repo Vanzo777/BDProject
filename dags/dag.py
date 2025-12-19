7999b9a1f995a104a47879b453f7e062a9e5ff2a
Vanzo777 <7asuvorov@mail.ru>
2025-12-20 02:55:46 +0300

    ф

--- dags/dag.py
+++ dags/dag.py
@@ -18,23 +18,27 @@ with DAG(
 
     create_iceberg_table = BashOperator(
         task_id="create_iceberg_table",
-        bash_command="""
-        spark-submit \
-          --master spark://spark-master:7077 \
-          --deploy-mode client \
-          --conf spark.driver.host=bdproject-airflow-worker-1 \
-          --conf spark.driver.bindAddress=0.0.0.0 \
-          --conf spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions \
-          --conf spark.sql.catalog.iceberg=org.apache.iceberg.spark.SparkCatalog \
-          --conf spark.sql.catalog.iceberg.type=hadoop \
-          --conf spark.sql.catalog.iceberg.warehouse=s3a://warehouse/ \
-          --conf spark.hadoop.fs.s3a.endpoint=http://minio-server:9000 \
-          --conf spark.hadoop.fs.s3a.access.key=minioadmin \
-          --conf spark.hadoop.fs.s3a.secret.key=minioadmin \
-          --conf spark.hadoop.fs.s3a.path.style.access=true \
-          --conf spark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem \
-          /opt/airflow/dags/scripts/create_iceberg_table.py
-        """
+        bash_command=r"""
+spark-submit \
+  --master spark://spark-master:7077 \
+  --deploy-mode client \
+  --conf spark.driver.host=bdproject-airflow-worker-1 \
+  --conf spark.driver.bindAddress=0.0.0.0 \
+  --conf spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions \
+  --conf spark.sql.catalog.iceberg=org.apache.iceberg.spark.SparkCatalog \
+  --conf spark.sql.catalog.iceberg.type=hadoop \
+  --conf spark.sql.catalog.iceberg.warehouse=s3a://warehouse/iceberg/ \
+  --conf spark.sql.catalog.iceberg.hadoop.fs.s3a.endpoint=http://minio-server:9000 \
+  --conf spark.sql.catalog.iceberg.hadoop.fs.s3a.access.key=minioadmin \
+  --conf spark.sql.catalog.iceberg.hadoop.fs.s3a.secret.key=minioadmin \
+  --conf spark.sql.catalog.iceberg.hadoop.fs.s3a.path.style.access=true \
+  --conf spark.sql.catalog.iceberg.hadoop.fs.s3a.connection.ssl.enabled=false \
+  --conf spark.sql.catalog.iceberg.hadoop.fs.s3a.aws.credentials.provider=org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider \
+  --conf spark.pyspark.python=python3 \
+  --conf spark.pyspark.driver.python=python3 \
+  --conf spark.executorEnv.PYSPARK_PYTHON=python3 \
+  /opt/airflow/dags/scripts/create_iceberg_table.py
+""",
     )
 
     create_iceberg_table
