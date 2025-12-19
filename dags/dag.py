dc63deced4de597231de58019ad1839638699d98
Vanzo777 <7asuvorov@mail.ru>
2025-12-20 01:30:26 +0300

    adding minoi

--- dags/dag.py
+++ dags/dag.py
@@ -1,30 +1,30 @@
 from datetime import datetime
-
 from airflow import DAG
-from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.providers.standard.operators.bash import BashOperator
 
+# Аргументы по умолчанию
 default_args = {
     "owner": "airflow",
     "start_date": datetime(2025, 1, 1),
+    "retries": 0,
 }
 
+# Определение DAG
 with DAG(
-    dag_id="test_spark_dag",
+    dag_id="test_spark_bash_cluster",
     default_args=default_args,
-    schedule=None,          # вместо schedule_interval
+    schedule=None,         # Запуск только вручную
     catchup=False,
-    tags=["test", "spark"],
+    tags=["test", "spark", "bash"],
 ) as dag:
 
-    run_spark = SparkSubmitOperator(
+    # Задача запуска Spark через BashOperator
+    # Это самый надежный способ, так как мы явно контролируем все аргументы
+    # и избегаем проблем с парсингом URL в SparkSubmitOperator
+    run_spark_pi = BashOperator(
         task_id="run_spark_pi",
-        application="/opt/spark/work-dir/test_spark_pi.py",  # твой PySpark-скрипт
-        conn_id="spark_default",                             # коннект в Airflow
-        verbose=True,
-        conf={
-            "spark.master": "spark://spark-master:7077",
-            "spark.app.name": "airflow_test_pi",
-        },
+        bash_command= "spark-submit pwd"
     )
 
-run_spark
+
+run_spark_pi
